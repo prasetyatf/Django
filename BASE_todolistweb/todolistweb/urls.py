82bce856f29cb28108309cf13ca9c7c2dc886227
prasetyatf <tegarfitriaji@gmail.com>
2023-05-22 20:52:42 +0700

    rename base dir. create index.html about.html

--- BASE_todolistweb/todolistweb/urls.py
+++ BASE_todolistweb/todolistweb/urls.py
@@ -22,5 +22,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
-    path('about', views.about)
+    path('about/', views.about)
 ]
--- BASE_todolistweb/todolistweb/views.py
+++ BASE_todolistweb/todolistweb/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1> Ini Index</h2>")
+    return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
